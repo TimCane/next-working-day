4c6c4515431b029758f1c4ef2206b08f9738a460
Tim Cane <3074314+TimCane@users.noreply.github.com>
2024-01-04 14:42:22 +0000

    replaced "isDevMode" with environment value

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
 import { AppComponent } from './app.component';
 import { NextWorkingDayModule } from './features/next-working-day/next-working-day.module';
 
@@ -18,7 +19,7 @@ import { NextWorkingDayModule } from './features/next-working-day/next-working-d
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
-      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      logOnly: environment.production, // Restrict extension to log-only mode
       autoPause: true,
     }),
   ],
